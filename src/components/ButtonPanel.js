f6d8cac95e96e570206dd7eaa55784c565c127cb
delicelydia <Delydia84@gmail.com>
2021-05-31 14:00:19 +0200

    Added group button to the button panel component

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,40 +1,43 @@
+import React from 'react';
 import PropTypes from 'prop-types';
 import Button from './Button';
 
-const ButtonPanel = () => {
-  return (
-    <div className="button-list">
-      <div className="group1">
-        <Button name="AC" />
-        <Button name="+/-" />
-        <Button name="%" />
-        <Button name="/" />
+class ButtonPanel extends React.Component {
+  render() {
+    return (
+      <div className="button-list">
+        <div className="group1">
+          <Button name="AC" />
+          <Button name="+/-" />
+          <Button name="%" />
+          <Button name="/" />
+        </div>
+        <div className="group2">
+          <Button name="7" />
+          <Button name="8" />
+          <Button name="9" />
+          <Button name="*" />
+        </div>
+        <div className="group3">
+          <Button name="4" />
+          <Button name="5" />
+          <Button name="6" />
+          <Button name="-" />
+        </div>
+        <div className="group4">
+          <Button name="1" />
+          <Button name="2" />
+          <Button name="3" />
+          <Button name="+" />
+        </div>
+        <div className="group5">
+          <Button name="0" />
+          <Button name="." />
+          <Button name="=" />
+        </div>
       </div>
-      <div className="group2">
-        <Button name="7" />
-        <Button name="8" />
-        <Button name="9" />
-        <Button name="*" />
-      </div>
-      <div className="group3">
-        <Button name="4" />
-        <Button name="5" />
-        <Button name="6" />
-        <Button name="-" />
-      </div>
-      <div className="group4">
-        <Button name="1" />
-        <Button name="2" />
-        <Button name="3" />
-        <Button name="+" />
-      </div>
-      <div className="group5">
-        <Button name="0" />
-        <Button name="." />
-        <Button name="=" />
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 ButtonPanel.propTypes = {
--- src/components/Display.js
+++ src/components/Display.js
@@ -1,17 +1,22 @@
+import React from 'react';
 import PropTypes from 'prop-types';
 
-const Display = ({value}) => (
-  <div>
-  {value}
-  </div>
-);
+class Display extends React.Component {
+  render() {
+    return (
+      <div className="display-board">
+        <h1 className="value">{this.props.value}</h1>
+      </div>
+    );
+  }
+}
 
 Display.propTypes = {
-  value: PropTypes.string.isRequired,
+  value: PropTypes.string,
 };
 
 Display.defaultProps = {
   value: '0',
-}
+};
 
 export default Display;
