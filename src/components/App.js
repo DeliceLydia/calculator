4046e72a1401958284ac71f621e43c783c6c9fa2
delicelydia <Delydia84@gmail.com>
2021-05-31 12:44:10 +0200

    Add app component

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import Display from './Display';
+import ButtonPanel from './ButtonPanel';
+
+const App = () => {
+  return (
+    <>
+      <Display />
+      <ButtonPanel />
+    </>
+  );
+};
+
+export default App;
