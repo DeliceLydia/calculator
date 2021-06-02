a6fc10d1c80a4ec354ca424f97a72b1da12fca4e
delicelydia <Delydia84@gmail.com>
2021-06-02 11:28:44 +0200

    Initialized state in the constructor
    - Add next, total, operation

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
+import { useState } from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 // eslint-disable-next-line no-unused-vars
 import calculate from '../logic/calculate';
 
-const App = () => (
-  <>
-    <Display />
-    <ButtonPanel />
-  </>
-);
+const App = () => {
+  const [total, setTotal] = useState(null);
+  const [next, setNext] = useState(null);
+  const [operation, setOperation] = useState(null);
+  return (
+    <div>
+      <>
+        <Display />
+        <ButtonPanel />
+      </>
+    </div>
+  );
+};
 
 export default App;
